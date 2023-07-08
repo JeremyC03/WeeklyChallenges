8dd086cf4561dbec10cb295dd5dc2a1c4d42a573
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-07-08 16:51:43 -0400

    Did Set 2 Challenges

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,143 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            if (letters.Contains(Char.ToLower(c)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            int elements = vals.Length;
+            if (elements % 2 == 0)
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if(number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            double max = double.MinValue;
+            double min = double.MaxValue;
+            foreach (double number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            return max + min;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+            else
+            {
+                return str1.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int answer = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                answer += numbers[i];
+            }
+            return answer;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int answer = 0;
+            foreach (int item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    answer += item;
+                }      
+            }
+            return answer;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            int sum = 0;
+            foreach (int item in numbers)
+            {  
+                    sum += item;
+            }
+            return (sum % 2 != 0);
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number / 2;
+            }
         }
     }
 }
